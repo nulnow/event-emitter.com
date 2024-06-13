efb5f933f7c62baaa0b9081fcbd1467bc7162f42
Разуваев Андрей <andrey.razuvaev@gazprombank.ru>
2024-06-13 14:26:25 +0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,87 @@
+import { promises as fs } from 'fs';
+
+import hljs from 'highlight.js';
+import javascript from 'highlight.js/lib/languages/javascript';
+import typescript from 'highlight.js/lib/languages/typescript';
 import Image from "next/image";
+import Link from "next/link";
+import src from './Octicons-mark-github.svg';
 
-export default function Home() {
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('typescript', typescript);
+
+
+const HEADER_HEIGHT_PX = 40;
+
+export default async function Home() {
+  const [
+    typescriptCode,
+    javascriptCode
+  ] = await Promise.all([
+    fs.readFile(process.cwd() + '/src/examples/typescript.txt', 'utf8'),
+    fs.readFile(process.cwd() + '/src/examples/javascript.txt', 'utf8')
+  ]);
+
+  const highlightedJavaScriptCode = hljs.highlight(
+    javascriptCode,
+    { language: 'javascript' }
+  ).value;
+
+  const highlightedTypeScriptCode = hljs.highlight(
+    typescriptCode,
+    { language: 'typescript' }
+  ).value;
+
+  const white = '#565656';
+  const keyword = '#d90c66';
+  const title = '#0098dc';
+  const cls = title; // '#d000ff';
+  const green = '#0db040';
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <title>event-emitter.com</title>
+      <style>{`
+        .hljs-keyword {
+          color: ${keyword};
+        }
+        
+        .hljs-title {
+          color: ${title};
+        }
+        
+        .hljs-title.class_ {
+          color: ${cls};
+        }
+        
+        .hljs-string, .hljs-comment {
+          color: ${green};
+        }
+      `}</style>
+      <header className="flex justify-between items-center px-4 sticky top-0 backdrop-blur-xl text-gray-700" style={{ height: HEADER_HEIGHT_PX }}>
+        <div>
+          <h1 className="font-light text-md md:text-2xl">event-emitter.com <span className="text-xs md:text-sm text-gray-600">With great power comes great responsibility</span></h1>
         </div>
+        <nav>
+          <Link href="https://github.com/nulnow">
+            <Image width={20} height={20} src={src} alt={"github logo"}></Image>
+          </Link>
+        </nav>
+      </header>
+      <div className="px-4 text-xs font-light">
+        Fast copy&paste solution
       </div>
+      <main className="flex flex-row flex-wrap justify-start items-start" style={{ minHeight: `calc(100svh - ${HEADER_HEIGHT_PX}px)` }}>
+        <section className="p-4" style={{ fontSize: 11, color: white }}>
+          <h3 className="text-black bg-yellow-300 p-1 rounded-md font-semibold mb-6">JavaScript</h3>
+          <pre className="font-mono" style={{ color: white }} dangerouslySetInnerHTML={{__html: highlightedJavaScriptCode}}></pre>
+        </section>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <section className=" p-4" style={{ fontSize: 11, color: white }}>
+          <h3 className="text-white p-1 rounded-md font-semibold mb-6" style={{ backgroundColor: '#3178c6' }}>TypeScript</h3>
+          <pre className="font-mono" style={{ color: white }} dangerouslySetInnerHTML={{__html: highlightedTypeScriptCode}}></pre>
+        </section>
+      </main>
+    </>
   );
 }
